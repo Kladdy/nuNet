eb3b669b7529aaed9f2d0b3cbbf8146a7e6b5d75
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-02-24 21:37:34 +0100

    Add runE18 and E19 for different amx weight

---
+++ training/runs/energy/runE18/evaluator.py
@@ -0,0 +1,66 @@
+# GPU allocation
+from gpuutils import GpuUtils
+GpuUtils.allocate(gpu_count=1, framework='keras')
+
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU')
+for device in physical_devices:
+    tf.config.experimental.set_memory_growth(device, True)
+# --------------
+
+# Imports
+import numpy as np
+from radiotools import plthelpers as php
+from matplotlib import pyplot as plt
+from tensorflow import keras
+import os
+import time
+from radiotools import helper as hp
+from NuRadioReco.utilities import units
+import pickle
+import argparse
+from termcolor import colored
+from toolbox import load_file, models_dir
+from constants import datapath, data_filename, label_filename, test_file_ids
+# -------
+
+# Parse arguments
+parser = argparse.ArgumentParser(description='Evaluate energy resolution')
+parser.add_argument("run_id", type=str ,help="the id of the run, eg '3.2' for run3.2")
+
+args = parser.parse_args()
+run_id = args.run_id
+
+# Save the run name and filename
+run_name = f"run{run_id}"
+filename = f"model_history_log_{run_name}.csv"
+
+# Models folder
+saved_model_dir = models_dir(run_name)
+
+print(colored(f"Evaluating energy resolution for {run_name}...", "yellow"))
+
+# Load the model
+model = keras.models.load_model(f'{saved_model_dir}/model.{run_name}.h5')
+
+# Load test file data and make predictions
+    # Load first file
+data, shower_energy_log10 = load_file(test_file_ids[0])
+
+    # Then load rest of files
+if len(test_file_ids) > 1:
+    for test_file_id in test_file_ids:
+        if test_file_id != test_file_ids[0]:
+            data_tmp, shower_energy_log10_tmp = load_file(test_file_id)
+
+            data = np.concatenate((data, data_tmp))
+            shower_energy_log10 = np.concatenate((shower_energy_log10, shower_energy_log10_tmp))
+
+shower_energy_log10_predict = model.predict(data)
+
+# Save predicted angles
+with open(f'{saved_model_dir}/model.{run_name}.h5_predicted.pkl', "bw") as fout:
+    pickle.dump([shower_energy_log10_predict, shower_energy_log10], fout, protocol=4)
+
+print(colored(f"Done evaluating energy resolution for {run_name}!", "green", attrs=["bold"]))
+print("")
---
+++ training/runs/energy/runE18/generator.py
@@ -0,0 +1,120 @@
+# Imports
+import os
+import numpy as np
+import tensorflow as tf
+import time
+from toolbox import load_file
+from constants import datapath, data_filename, label_filename, n_files, n_files_val, dataset_name, dataset_em, dataset_noise
+# -------
+
+np.set_printoptions(precision=4)
+
+# n_files and n_files_val comes from dataset in constants.py
+n_files_test = 3
+norm = 1e-6
+
+n_files_train = n_files - n_files_val - n_files_test
+list_of_file_ids_train = np.arange(n_files_train, dtype=np.int)
+list_of_file_ids_val = np.arange(n_files_train, n_files_train + n_files_val, dtype=np.int)
+list_of_file_ids_test = np.arange(n_files_train + n_files_val, n_files, dtype=np.int)
+n_events_per_file = 100000
+batch_size = 64
+
+print(f"training on {n_files_train} files ({n_files_train/n_files*100:.1f}%), validating on {n_files_val} files ({n_files_val/n_files*100:.1f}%), testing on {n_files_test} files ({n_files_test/n_files*100:.1f}%)")
+
+
+# WEIGHTS - WEIGHING BY ENERGY DISTRIBUTION
+MAX_WEIGHT = 5
+
+if dataset_name == "ALVAREZ" and dataset_em == False and dataset_noise == True:
+    dataset_to_use = "ALVAREZ-HAD"
+    
+if dataset_name == "ARZ" and dataset_em == False and dataset_noise == True:
+    dataset_to_use = "ARZ-HAD"
+
+if dataset_name == "ARZ" and dataset_em == True and dataset_noise == True:
+    dataset_to_use = "ARZ-EM"
+    
+with open(f"weights/{dataset_to_use}_weights.npy", "rb") as f:  
+    file_contents = np.load(f)
+    WEIGHTING_energy_list = file_contents[:, 0]
+    WEIGHTING_weight_list = file_contents[:, 1]
+    WEIGHTING_count_list = file_contents[:, 2]
+    
+print("energies: ", WEIGHTING_energy_list)
+print("weights: ", WEIGHTING_weight_list)
+
+# https://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array
+def find_nearest_and_return_index(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
+
+def get_weight_by_log10_shower_energy(log10_shower_energy):
+    nearest_idx = find_nearest_and_return_index(WEIGHTING_energy_list, log10_shower_energy)
+
+    weight_for_nearest_idx = WEIGHTING_weight_list[nearest_idx]
+    if weight_for_nearest_idx > MAX_WEIGHT:
+        return MAX_WEIGHT
+    else:
+        return weight_for_nearest_idx
+
+
+class TrainDataset(tf.data.Dataset):
+
+    def _generator(file_id):
+        if((file_id + 1) == n_files_train):
+#             print("reshuffling")
+            np.random.shuffle(list_of_file_ids_train)
+
+        # Opening the file
+        i_file = list_of_file_ids_train[file_id]
+        data, shower_energy_log10 = load_file(i_file, norm)
+        num_samples = data.shape[0]
+        rand_ids = np.arange(num_samples, dtype=np.int)
+        np.random.shuffle(rand_ids)
+        n_batches = num_samples // batch_size
+        for i_batch in range(n_batches):
+            # Reading data (line, record) from the file
+            y = shower_energy_log10[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size]]
+            x = data[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size], :, :, :]
+            # calculate weights for all the targets
+            sample_weights = [get_weight_by_log10_shower_energy(target) for target in y]
+            yield x, y, sample_weights
+
+    def __new__(cls, file_id):
+        return tf.data.Dataset.from_generator(
+            cls._generator,
+            output_types=(tf.dtypes.float64, tf.dtypes.float64, tf.dtypes.float64),
+            output_shapes=((batch_size, 5, 512, 1), (batch_size, ), (batch_size, )),
+            args=(file_id,)
+        )
+
+
+class ValDataset(tf.data.Dataset):
+
+    def _generator(file_id):
+        if((file_id + 1) == n_files_val):
+            np.random.shuffle(list_of_file_ids_val)
+
+        # Opening the file
+        i_file = list_of_file_ids_val[file_id]
+        data, shower_energy_log10 = load_file(i_file, norm)
+        num_samples = data.shape[0]
+        rand_ids = np.arange(num_samples, dtype=np.int)
+        np.random.shuffle(rand_ids)
+        n_batches = num_samples // batch_size
+        for i_batch in range(n_batches):
+            # Reading data (line, record) from the file
+            y = shower_energy_log10[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size]]
+            x = data[rand_ids[i_batch * batch_size:(i_batch + 1) * batch_size], :, :, :]
+            yield x, y
+
+    def __new__(cls, file_id):
+        return tf.data.Dataset.from_generator(
+            cls._generator,
+            output_types=(tf.dtypes.float64, tf.dtypes.float64),
+            output_shapes=((batch_size, 5, 512, 1), (batch_size, )),
+            args=(file_id,)
+        )
+
